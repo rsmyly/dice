35c1dd4b8883fe73ca7ff6f9f08e3bc84ec58ef9
Robert Smyly <robert@smyly.net>
2023-02-11 10:27:46 -0600

    replace random choice placeholder with ellipses

--- src/components/Die/Die.tsx
+++ src/components/Die/Die.tsx
@@ -1,50 +1,30 @@
-import { getRandomElementFromArray } from "helpers/random";
 import { useEffect, useState } from "react";
 
-import { ChoiceList, DieConfiguration } from "../../types";
+import { DieConfiguration } from "../../types";
 import { useChooserWithDelay } from "../hooks/useChooser";
 import styles from "./Die.module.css";
 
 export type DieProps = DieConfiguration;
 
 // TODO: this whole placeholder thing has become a mess
-const defaultPlacholderIterationTimeMs = 75;
-const DieResultPlaceholder = ({ choices }: { choices: ChoiceList }) => {
-  const [placeholder, setPlaceholder] = useState("");
-
-  const updatePlaceHolder = () => {
-    const otherChoices = choices.filter((choice) => choice !== placeholder);
-
-    const newPlaceHolder =
-      otherChoices.length === 1
-        ? otherChoices[0]
-        : getRandomElementFromArray(otherChoices);
-
-    setPlaceholder(newPlaceHolder);
-  };
+const defaultPlacholderTimePerFrameMs = 340;
+const maxDotCount = 3;
+const DieResultPlaceholder = () => {
+  const [currentDotCount, setCurrentDotCount] = useState(0);
 
   useEffect(() => {
-    let lastUpdated = new Date();
-
-    updatePlaceHolder();
     const intervalPointer = setInterval(() => {
-      if (
-        new Date().getTime() - lastUpdated.getTime() >
-        defaultPlacholderIterationTimeMs
-      ) {
-        lastUpdated = new Date();
-        updatePlaceHolder();
-      }
-    }, defaultPlacholderIterationTimeMs / 5);
+      setCurrentDotCount((count) => (count + 1) % maxDotCount);
+    }, defaultPlacholderTimePerFrameMs);
 
-    return () => {
-      clearInterval(intervalPointer);
-    };
+    return () => clearInterval(intervalPointer);
+  });
 
-    // eslint-disable-next-line
-  }, [...choices]);
+  const displayString = ".".repeat(currentDotCount + 1);
 
-  return <span>{placeholder}</span>;
+  return (
+    <span style={{ textAlign: "left", width: "4em" }}>{displayString}</span>
+  );
 };
 
 const defaultDelayTimeMs = 1000 * 1.0;
@@ -56,11 +36,7 @@ const Die = ({ name, choices }: DieProps) => {
 
   const [isBeingPushed, setIsBeingPushed] = useState(false);
 
-  const choiceDisplay = isChoicePending ? (
-    <DieResultPlaceholder choices={choices} />
-  ) : (
-    choice
-  );
+  const choiceDisplay = isChoicePending ? <DieResultPlaceholder /> : choice;
 
   return (
     <div
