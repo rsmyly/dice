c0b6dbecf4dfa0d50542e14f5d6f1a0b63104e76
Robert Smyly <robert@smyly.net>
2023-02-18 10:19:25 -0600

    add basics for tabbed navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import NavContainer from "components/NavContainer.tsx";
 import "./App.css";
 import DiceTray from "./components/DiceTray";
 
+const tabs = [
+  {
+    name: "tray",
+    content: (
+      <>
+        <h1>dice</h1>
+        <DiceTray></DiceTray>
+      </>
+    ),
+  },
+  {
+    name: "test",
+    content: <div>hello world</div>,
+  },
+];
+
 function App() {
-  return (
-    <>
-      <h1>dice</h1>
-      <DiceTray></DiceTray>
-    </>
-  );
+  return <NavContainer tabs={tabs} />;
 }
 
 export default App;
---
+++ src/components/NavContainer.tsx/NavContainer.tsx
@@ -0,0 +1,50 @@
+import { Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+
+type NavContainerTabConfig = {
+  name: string;
+  content: React.ReactNode;
+};
+
+type NavContainerProps = {
+  tabs: NavContainerTabConfig[];
+};
+
+const NavContainer = ({ tabs }: NavContainerProps) => {
+  const [currentTabName, setCurrentTabName] = useState(tabs[0].name);
+
+  const handleTabChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    value: string
+  ) => {
+    setCurrentTabName(value);
+  };
+
+  return (
+    <>
+      <Tabs
+        value={currentTabName}
+        onChange={handleTabChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        {tabs.map(({ name }) => (
+          <Tab label={name} key={name} value={name} />
+        ))}
+      </Tabs>
+      <hr />
+      {tabs.map((tab) => (
+        <div
+          key={tab.name}
+          style={{ display: tab.name === currentTabName ? "block" : "none" }}
+        >
+          {tab.content}
+        </div>
+      ))}
+      ;
+    </>
+  );
+};
+
+export default NavContainer;
